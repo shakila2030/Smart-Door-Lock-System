a52a51cc98eace6126f10a8480e0b8e91c660cdf
Sandarekha-Gunasiri <sandarekhagunasiri@gmail.com>
2025-02-23 23:06:33 -0800

    home page

--- SmartDoorLockSystem/App.js
+++ SmartDoorLockSystem/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-
-const Stack = createStackNavigator();
+import React from "react";
+import { SafeAreaView } from "react-native";
+import HomePage from "./app/HomePage";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <HomePage />
+    </SafeAreaView>
   );
 }
